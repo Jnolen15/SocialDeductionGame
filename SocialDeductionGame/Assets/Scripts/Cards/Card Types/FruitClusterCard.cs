ad5b9ab6d0819a2de8145ba93744a6c024f37ab9
Jared Nolen <60532581+Jnolen15@users.noreply.github.com>
2023-09-22 16:34:47 -0700

    Reworked Fruit Cluster
    Changed fruit cluster. Now instead of giving you multiple cards on use, it acts like a normal piece of fruit. However, upon use it gives the player a new card.
    Fruit cluster (3) => Fruit cluster (2) => fruit.
    This makes it more useful with reduced inventory space. And less powerful for the food collection night event.

--- SocialDeductionGame/Assets/Scripts/Cards/Card Types/FruitClusterCard.cs
+++ SocialDeductionGame/Assets/Scripts/Cards/Card Types/FruitClusterCard.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class FruitClusterCard : Card
 {
     [Header("Cluster Details")]
+    [SerializeField] private float _servings;
     [SerializeField] private int _givenCardID;
     private CardManager _cardManager;
 
+    // Fruit cluster acts like a 0.5 serving food. But each time its used it gives the player the next fruit card
+    // Fruit cluster (3) => Fruit cluster (2) => Fruit
     public override void OnPlay(GameObject playLocation)
     {
-        PlayerObj player = playLocation.GetComponent<PlayerObj>();
+        _cardManager = GameObject.FindGameObjectWithTag("CardManager").GetComponent<CardManager>();
 
-        if (player != null)
+        PlayerObj player = playLocation.GetComponent<PlayerObj>();
+        Campfire campfire = playLocation.GetComponent<Campfire>();
+
+        if (campfire != null)
         {
-            _cardManager = GameObject.FindGameObjectWithTag("CardManager").GetComponent<CardManager>();
-            int rand = Random.Range(2, 4);
-            for(int i = 0; i < rand; i++)
-                _cardManager.GiveCard(_givenCardID);
+            Debug.Log("Cooking food card. Adding " + _servings + " Servings");
+            campfire.AddFood(_servings);
+
+            _cardManager.GiveCard(_givenCardID);
+        }
+        else if (player != null)
+        {
+            Debug.Log($"Player eating {_servings} servings");
+            player.Eat(_servings, 0);
+
+            _cardManager.GiveCard(_givenCardID);
         }
         else
         {
