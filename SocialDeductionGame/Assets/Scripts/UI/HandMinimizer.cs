d8a0d887223a9bc742ad9b5039f089bb2818745b
Jared Nolen <60532581+Jnolen15@users.noreply.github.com>
2023-09-29 15:33:22 -0700

    Player Hand minimizing
    - The players hand now minimizes when not interacting with it
    - Moving your mouse down to the hand will bring it back up
    - Then after a second away it will go back down

---
+++ SocialDeductionGame/Assets/Scripts/UI/HandMinimizer.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+public class HandMinimizer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    // =================== Refrences ===================
+    [SerializeField] private Transform _hand;
+    [SerializeField] private Transform _defaultPos;
+    [SerializeField] private Transform _minimizedPos;
+    [SerializeField] private bool _minimized;
+    [SerializeField] private bool _hovering;
+
+    [SerializeField] private float _bufferTimerMax;
+    [SerializeField] private float _bufferTimer;
+
+    // =================== Function ===================
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointer Entered");
+        _hovering = true;
+        _bufferTimer = _bufferTimerMax;
+
+        if (_minimized)
+            Maximize();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (_minimized)
+            return;
+
+        Debug.Log("Pointer Exit");
+
+        _hovering = false;
+        _bufferTimer = _bufferTimerMax;
+    }
+
+    private void Maximize()
+    {
+        _minimized = false;
+        //_hand.position = _defaultPos.position;
+        _hand.DOMoveY(_defaultPos.position.y, 0.2f).SetEase(Ease.InSine);
+    }
+
+    private void Minimize()
+    {
+        _minimized = true;
+        //_hand.position = _minimizedPos.position;
+        _hand.DOMoveY(_minimizedPos.position.y, 0.2f).SetEase(Ease.InSine);
+    }
+
+    private void Update()
+    {
+        if (_hovering)
+            return;
+
+        if (_bufferTimer > 0)
+            _bufferTimer -= Time.deltaTime;
+        else if (!_minimized)
+            Minimize();
+    }
+}
