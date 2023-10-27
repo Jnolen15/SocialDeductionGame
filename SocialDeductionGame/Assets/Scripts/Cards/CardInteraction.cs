0e7cdf31491f9b0708e5987d993e7251786c0617
Jared Nolen <Legodoghead@gmail.com>
2023-10-27 14:32:42 -0700

    Small tweaks and fixes
    - Removed space from submit keybinding
    - Adjusted Player UI hunger/health number to better fit when at intervals of .5
    - Increased afternoon time from 60 -> 80
    - Fixed card drag icons sometimes staying after drag. (Now only left mouse button can drag)

--- SocialDeductionGame/Assets/Scripts/Cards/CardInteraction.cs
+++ SocialDeductionGame/Assets/Scripts/Cards/CardInteraction.cs
@@ -16,6 +16,7 @@ public class CardInteraction : MonoBehaviour,
     [SerializeField] private HandManager _handManager;
     [SerializeField] private PlayerCardManager _playerCardManager;
     private GameObject _indicator;
+    private bool _dragging;
 
     // =============== Setup ===============
     void Start()
@@ -35,21 +36,35 @@ public class CardInteraction : MonoBehaviour,
     #region Card Drag
     public void OnBeginDrag(PointerEventData eventData)
     {
+        if (eventData.button != PointerEventData.InputButton.Left)
+        {
+            Debug.Log("Not primary click");
+            return;
+        }
+
         Canvas parentCanvas = gameObject.GetComponentInParent<Canvas>();
         _indicator = Instantiate(_dragIcon, parentCanvas.transform);
+        _dragging = true;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
+        if (!_dragging)
+            return;
+
         _indicator.transform.position = eventData.position;
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        if (!_dragging)
+            return;
+
         _playerCardManager.TryCardPlay(_card);
 
         Destroy(_indicator);
         _indicator = null;
+        _dragging = false;
     }
     #endregion
 }
