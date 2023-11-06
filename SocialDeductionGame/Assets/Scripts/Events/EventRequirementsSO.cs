e3e33142fc4890c6a732236f891eacab6adcc25a
Jared Nolen <legodoghead@gmail.com>
2023-11-06 11:52:16 -0800

    Balance Changes
    - Useless rate from 30 -> 50
    - Useless debuff mod 15 ->25
    - Tweaked location drop tables
    -- Location primary resource is most common at 50% then small food at 30%
    - Increased event resources needed at all tiers of players

--- SocialDeductionGame/Assets/Scripts/Events/EventRequirementsSO.cs
+++ SocialDeductionGame/Assets/Scripts/Events/EventRequirementsSO.cs
@@ -23,17 +23,17 @@ public class EventRequirementsSO : ScriptableObject
         switch (numPlayers)
         {
             case 6:
-                return new Vector2(3, 3);
+                return new Vector2(5, 4); //9 //Old: 3, 3
             case 5:
-                return new Vector2(3, 2);
+                return new Vector2(4, 3); //7 //Old: 3, 2
             case 4:
-                return new Vector2(2, 2);
+                return new Vector2(3, 3); //6 //Old: 2, 2
             case 3:
-                return new Vector2(2, 1);
+                return new Vector2(2, 2); //4 //Old: 2, 1
             case 2:
-                return new Vector2(2, 0);
+                return new Vector2(2, 1); //3 //Old: 2, 0
             case 1:
-                return new Vector2(1, 0);
+                return new Vector2(1, 1); //2 //Old: 1, 0
             default:
                 Debug.LogError("GetRequirements called with not correct number of players: " + numPlayers);
                 return new Vector2(0, 0);
