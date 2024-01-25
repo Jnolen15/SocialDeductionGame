0f34d8e3465929730ae08b13ba8cd4e998d630a9
Jared Nolen <legodoghead@gmail.com>
2024-01-25 11:32:53 -0800

    Basic networked chat messages
    - TextChatManager is now network object
    - Basic networked text chat function
    - TCM has custom class for messages that include message, sender name, and channel enum
    - Text bubble now also recieves a name and location
    - some Text chat UI updates to show what channel you are typing to + buttons for testing

--- SocialDeductionGame/Assets/Scripts/UI/TextChatManager.cs
+++ SocialDeductionGame/Assets/Scripts/UI/TextChatManager.cs
@@ -1,41 +1,117 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 using TMPro;
 
-public class TextChatManager : MonoBehaviour
+public class TextChatManager : NetworkBehaviour
 {
     // =============== Setup ===============
     [SerializeField] private TMP_InputField _messageInputField;
     [SerializeField] private Transform _messageContent;
+    [SerializeField] private GameObject _saboChatIcon;
+    [SerializeField] private GameObject _deathChatIcon;
     [SerializeField] private GameObject _textMessagePref;
 
     // just for testing
     [SerializeField] private bool _saboChat;
     [SerializeField] private bool _deadChat;
 
+    public enum ChatChannel 
+    {
+        Camp,
+        Beach,
+        Forest,
+        Plateau,
+        Saboteur,
+        Dead
+    }
+
+
+    public class ChatMessage : INetworkSerializable
+    {
+        public string MSG;
+        public string SenderName;
+        public ChatChannel Channel;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            if (serializer.IsReader)
+            {
+                var reader = serializer.GetFastBufferReader();
+                reader.ReadValueSafe(out MSG);
+                reader.ReadValueSafe(out SenderName);
+                reader.ReadValueSafe(out Channel);
+            }
+            else
+            {
+                var writer = serializer.GetFastBufferWriter();
+                writer.WriteValueSafe(MSG);
+                writer.WriteValueSafe(SenderName);
+                writer.WriteValueSafe(Channel);
+            }
+        }
+
+        public ChatMessage()
+        {
+            MSG = "Message";
+            SenderName = "Sender";
+            Channel = ChatChannel.Camp;
+        }
+
+        public ChatMessage(string msg, string sender, ChatChannel channel)
+        {
+            MSG = msg;
+            SenderName = sender;
+            Channel = channel;
+        }
+    }
+
+
     // =============== Function ===============
+    public void ToggleSaboChat()
+    {
+        _saboChat = !_saboChat;
+
+        _saboChatIcon.SetActive(_saboChat);
+    }
+
+    public void ToggleDeathChat()
+    {
+        _deadChat = !_deadChat;
+
+        _deathChatIcon.SetActive(_deadChat);
+    }
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            SendChatMessage(_messageInputField.text);
+            if (!string.IsNullOrWhiteSpace(_messageInputField.text))
+            {
+                ChatMessage newMessage = new ChatMessage(_messageInputField.text, "Player", ChatChannel.Camp);
+                SendChatMessageServerRpc(newMessage);
+            }
+
             _messageInputField.text = "";
         }
     }
 
-    private void SendChatMessage(string msg)
+    [ServerRpc(RequireOwnership = false)]
+    private void SendChatMessageServerRpc(ChatMessage msg)
     {
-        if (string.IsNullOrWhiteSpace(msg)) return;
+        RecieveChatMessageClientRpc(msg);
+    }
+
+    [ClientRpc]
+    private void RecieveChatMessageClientRpc(ChatMessage msg)
+    {
+        Debug.Log("Message recieved: " + msg.MSG);
 
         TextChatMessage chatMsg = Instantiate(_textMessagePref, _messageContent).GetComponent<TextChatMessage>();
 
-        if(_saboChat)
-            chatMsg.Setup(msg, "sabo");
-        else if (_deadChat)
-            chatMsg.Setup(msg, "dead");
-        else
-            chatMsg.Setup(msg);
+        //chatMsg.Setup(msg, "player", ChatChannel.Camp);
+        chatMsg.Setup(msg.MSG, msg.SenderName, msg.Channel);
 
         chatMsg.transform.SetAsFirstSibling();
     }
--- SocialDeductionGame/Assets/Scripts/UI/TextChatMessage.cs
+++ SocialDeductionGame/Assets/Scripts/UI/TextChatMessage.cs
@@ -7,19 +7,21 @@ public class TextChatMessage : MonoBehaviour
 {
     // =============== Refrences ===============
     [SerializeField] private TextMeshProUGUI _messageText;
+    [SerializeField] private TextMeshProUGUI _nameText;
     [SerializeField] private Color _saboColor;
     [SerializeField] private Color _deadColor;
 
     // =============== Setup ===============
-    public void Setup(string message, string chat = null)
+    public void Setup(string message, string senderName, TextChatManager.ChatChannel channel)
     {
         _messageText.text = message;
+        _nameText.text = senderName;
 
-        if(chat == "sabo")
+        if (channel == TextChatManager.ChatChannel.Saboteur)
         {
             _messageText.color = _saboColor;
         }
-        else if (chat == "dead")
+        else if (channel == TextChatManager.ChatChannel.Dead)
         {
             _messageText.color = _deadColor;
         }
